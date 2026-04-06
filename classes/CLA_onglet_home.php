884e579e91d4613c674e79e72e2b00fddd8b11e6
Sylverscal <sccd@sylverscal.net>
2026-04-06 12:14:10 +0200

    Création projet de liste de courses MklAnj

--- classes/CLA_onglet_home.php
+++ classes/CLA_onglet_home.php
@@ -14,13 +14,26 @@ class CLA_onglet_home extends CLA_onglet_principal {
      */
     #[\Override]
     final function affiche() {
-?>
+        $chemin_image = "https://mklanj:8890/image/Liste_courses_MichalAnge.jpg";
+    ?>
         <div class="w3-container">
             <div class="w3-panel ">
                 <div class="w3-tag w3-xlarge w3-padding-large w3-round-large w3-yellow w3-text-red w3-center">
                     LISTE DES COURSES
                 </div>
             </div>
+            <div class="w3-card-4">
+                <div class="w3-container">
+                    <p>Liste de courses faite par Michel Ange à un serviteur
+                        <br>
+                        Avec des dessins car il était analphabète.
+                        <br>
+                        1518
+                    </p>
+                </div>
+                <img src="<?php echo $chemin_image; ?>" alt="Liste de courses de Michel Ange">
+            </div>
+            
         </div>
         <?php
     }
