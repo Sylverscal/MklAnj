8cc971b42a5bf40655cdc6e603aaa41a081feb31
Sylverscal <sccd@sylverscal.net>
2026-06-09 08:30:57 +0200

    Amélioration présentation de la rceherche

--- classes/CLA_Recherche.php
+++ classes/CLA_Recherche.php
@@ -15,12 +15,12 @@ class CLA_Recherche {
         ?>
         <div class="w3-container w3-lime w3-padding">
             <div class="w3-row">
-                <div class="w3-container w3-threequarter">
+                <div class="w3-col w3-right" style="width: 50px">
+                    <button id="BTN_RCH_RAZ" class="w3-button w3-green w3-border w3-large w3-ripple w3-circle w3-right"><i class="fa fa-eraser" aria-hidden="true"></i></button>
+                </div>
+                <div class="w3-rest">
                     <input id="INP_RCH" class="w3-input" type="text" placeholder="Taper le nom de la course à chercher">
                     
-                </div>
-                <div class="w3-container w3-rest">
-                    <button id="BTN_RCH_RAZ" class="w3-button w3-green w3-border w3-large w3-ripple w3-circle w3-right"><i class="fa fa-eraser" aria-hidden="true"></i></button>
                 </div>
             </div>
         </div>
